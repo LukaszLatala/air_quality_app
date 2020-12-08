16e90d68248abd1743e5377493769c1ed716e68b
Łukasz Latała <lukaszlatala@MacBook-Pro-ukasz.local>
2020-12-08 20:56:43 +0100

    initial commit

---
+++ src/Api/apiKey.js
@@ -0,0 +1,1 @@
+export const apiKey = process.env.REACT_APP_AIR_QUALITY_API_KEY;
--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import { apiKey } from "./Api/apiKey";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const cityData = [
+  {
+    name: "Kraków",
+    cords: {
+      lat: "50.062006",
+      lng: "19.940984",
+    },
+  },
+  {
+    name: "Warszawa",
+    cords: {
+      lat: "52.229676",
+      lng: "21.012229",
+    },
+  },
+];
+
+class App extends Component {
+  state = {
+    cities: [],
+  };
+
+  getData = (e) => {
+    e.preventDefault();
+
+    const cityCords = JSON.parse(e.target.citySelect.value);
+
+    axios
+      .get(
+        `https://airapi.airly.eu/v2/measurements/point?lat=${cityCords.lat}&lng=${cityCords.lng}`,
+        {
+          headers: {
+            Accept: "application/json",
+            apikey: apiKey,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          cities: [...res.data.current.values],
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.getData}>
+          <select name="citySelect" id="">
+            {cityData.map((city) => {
+              const jsonCords = JSON.stringify(city.cords);
+
+              return <option value={jsonCords}>{city.name}</option>;
+            })}
+          </select>
+          <button type="submit">search</button>
+        </form>
+
+        <div>
+          {this.state.cities.map((el) => (
+            <ul>
+              <li>
+                {el.name} - {el.value}
+              </li>
+            </ul>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// select
+// https://material-ui.com/components/text-fields/
